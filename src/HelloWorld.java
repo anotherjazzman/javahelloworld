95bef90aca7a6f0974dffd71f0130d45e13f6081
anotherjazzman <anotherjazzman@yahoo.com>
2016-11-22 16:21:08 +0000

    Update

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main (String [] args) {
-		System.out.println("Hello World - Live is Wonderful");
+		System.out.println("Hello World - Life is Wonderful");
 	}
 }
